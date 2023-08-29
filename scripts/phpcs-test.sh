49c4d155f8f90bc7a0fd7200df907bd744c621c7
aneel2023 <developer.anil88@gmail.com>
2023-08-29 20:38:12 +0500

    Added custom script for phpcs

--- scripts/phpcs-test.sh
+++ scripts/phpcs-test.sh
@@ -7,4 +7,4 @@ PROJECT_PATH="$(pwd)"
 cd "$PROJECT_PATH/magento"
 
 echo "Execute PHPCS FOR APP";
-./magento/build/tools/bin/phpcs --standard=$INPUT_STANDARD $PROJECT_PATH/magento/app/code/$INPUT_EXTENSION
+build/tools/bin/phpcs --standard=$INPUT_STANDARD $PROJECT_PATH/magento/app/code/$INPUT_EXTENSION
