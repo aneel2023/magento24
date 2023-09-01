d95ae17faacbe2496f7e76dbb4ddf84f96b825cf
aneel2023 <developer.anil88@gmail.com>
2023-09-01 16:43:14 +0500

    Added phpmd

--- scripts/mess-detector.sh
+++ scripts/mess-detector.sh
@@ -6,28 +6,13 @@ echo "currently in $PROJECT_PATH"
 
 cd "$PROJECT_PATH/magento"
 
-
-/usr/local/bin/composer install --dry-run --prefer-dist --no-progress &> /dev/null
-
-COMPOSER_COMPATIBILITY=$?
-
-echo "Composer compatibility: $COMPOSER_COMPATIBILITY"
-
-set -e
-
-if [ $COMPOSER_COMPATIBILITY = 0 ]
-then
-	/usr/local/bin/composer install --prefer-dist --no-progress
-else
-  echo "using composer v1"
-  php7.2 /usr/local/bin/composer self-update --1
-	/usr/local/bin/composer install --prefer-dist --no-progress
-fi
+/usr/local/bin/composer install --prefer-dist --no-progress
+chmod +x $PROJECT_PATH/magento/bin/magento
 
 if [ ! -f $INPUT_RULESET ]
 then
   echo -e "\e[32mThe ruleset file [$INPUT_RULESET] NOT FOUND\e[0m"
-  echo "Using default magento ruleset dev/tests/static/testsuite/Magento/Test/Php/_files/phpmd/ruleset.xml"
+  echo "Using default magento ruleset $PROJECT_PATH/magento/dev/tests/static/testsuite/Magento/Test/Php/_files/phpmd/ruleset.xml"
   cp /opt/config/defaults/ruleset.xml .
   INPUT_RULESET=ruleset.xml
 fi
@@ -37,11 +22,10 @@ then
 	echo "PHPMD BIN ###### $PROJECT_PATH/magento/build/tools/bin exists."
 fi
 
-
 if [ -n $INPUT_MD_SRC_PATH ]
 then
   echo -e "\e[32mMess detection initiated\e[0m"
-  php build/tools/bin/phpmd $INPUT_MD_SRC_PATH ansi $INPUT_RULESET
+  php $PROJECT_PATH/magento/build/tools/bin/phpmd $INPUT_MD_SRC_PATH ansi $PROJECT_PATH/magento/$INPUT_RULESET
 else
   echo -e "\e[31mPlease specify the $md_src_path\e[0m"
   exit 1;
