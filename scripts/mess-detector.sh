8d7275582d71c31014e78bf580f63c874ac76847
aneel2023 <developer.anil88@gmail.com>
2023-09-01 15:44:56 +0500

    Added phpmd

---
+++ scripts/mess-detector.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+PROJECT_PATH="$(pwd)"
+
+if [ -d "$PROJECT_PATH/magento-coding-standard" ]
+then
+	echo "Directory $PROJECT_PATH/magento-coding-standard already exists."
+else
+	composer create-project magento/magento-coding-standard --stability=dev magento-coding-standard
+fi
+
+
+cd $PROJECT_PATH/magento-coding-standard
+
+if [ -d "$PROJECT_PATH/magento/build/tools" ]
+then
+	echo "PHPMD $PROJECT_PATH/magento/build/tools exists."
+	cd $PROJECT_PATH/magento/build/tools
+	composer install
+	cd $PROJECT_PATH/magento-coding-standard
+fi
+
+if [ -d "$PROJECT_PATH/magento/build/tools/bin" ]
+then
+	echo "PHPMD BIN ###### $PROJECT_PATH/magento/build/tools/bin exists."
+fi
+
+if [ -d "$PROJECT_PATH/magento/app/code/$INPUT_EXTENSION" ]
+then
+	echo "Extension $PROJECT_PATH/magento/app/code/$INPUT_EXTENSION exists."
+        $PROJECT_PATH/magento/build/tools/bin/phpmd text $PROJECT_PATH/magento/$INPUT_STANDARD $PROJECT_PATH/magento/app/code/$INPUT_EXTENSION
+elif [ -d "$PROJECT_PATH/$INPUT_EXTENSION" ]
+then
+	echo "Directory $PROJECT_PATH / $INPUT_EXTENSION exists."
+        $PROJECT_PATH/magento/build/tools/bin/phpmd text $PROJECT_PATH/magento/$INPUT_STANDARD $PROJECT_PATH/$INPUT_EXTENSION
+else
+	echo "Error: Directory $PROJECT_PATH/magento/app/code/$INPUT_EXTENSION  does not exists."
+	echo "Nor does the Directory $PROJECT_PATH/$INPUT_EXTENSION ."
+fi
