64f83693e53985344a2b7d868bcce30f44a5f95e
aneel2023 <developer.anil88@gmail.com>
2023-09-01 16:55:34 +0500

    Added phpmd

--- scripts/mess-detector.sh
+++ scripts/mess-detector.sh
@@ -6,8 +6,22 @@ echo "currently in $PROJECT_PATH"
 
 cd "$PROJECT_PATH/magento"
 
-/usr/local/bin/composer install --prefer-dist --no-progress
-chmod +x $PROJECT_PATH/magento/bin/magento
+/usr/local/bin/composer install --dry-run --prefer-dist --no-progress &> /dev/null
+
+COMPOSER_COMPATIBILITY=$?
+
+echo "Composer compatibility: $COMPOSER_COMPATIBILITY"
+
+set -e
+
+if [ $COMPOSER_COMPATIBILITY = 0 ]
+then
+	/usr/local/bin/composer install --prefer-dist --no-progress
+else
+  echo "using composer v1"
+  php7.2 /usr/local/bin/composer self-update --1
+	/usr/local/bin/composer install --prefer-dist --no-progress
+fi
 
 if [ ! -f $INPUT_RULESET ]
 then
